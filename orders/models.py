37f2b35d216627d6a0d72e8fccc19cc006959d5b
Srejus Jayakumar <82509096+srejus@users.noreply.github.com>
2025-07-31 23:39:04 +0530

    15998

--- orders/models.py
+++ orders/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Order(models.Model):
+    order_id = models.CharField(max_length=100,unique=True)
+    note = models.TextField(null=True,blank=True)
+    total_order_amount = models.FloatField(default=0.0)
+    customer_name = models.CharField(max_length=150,null=True,blank=True)
+    created_at = models.DateTimeField(auto_now_add=True)
+
