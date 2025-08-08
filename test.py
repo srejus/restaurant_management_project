019bb53f0475a91856a54ebbf295ea7d1a91773b
Srejus Jayakumar <82509096+srejus@users.noreply.github.com>
2025-08-08 13:27:11 +0530

    70

--- test.py
+++ test.py
@@ -1,10 +1,18 @@
-x =10
-i = 0
+def add_nums(a,b):
+    return a+b
 
-c = 101
 
-x = 00
+def multi(a,b):
+    return a*b
 
-c =0
+c =s
 
-x = 000
+from .t1 import sayHello
+
+def say():
+    name = "ram"
+    res = sayHello(name)
+    return res
+
+
+cv = 00
